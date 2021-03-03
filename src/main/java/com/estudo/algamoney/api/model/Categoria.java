ef3661b55d729815c5910d6b575c6b9ea9d92b4c
Murilo Martins da Silva <murilo.silva@spread.com.br>
2021-03-03 11:28:56 -0300

    Criando exception e post Categoria

--- src/main/java/com/estudo/algamoney/api/model/Categoria.java
+++ src/main/java/com/estudo/algamoney/api/model/Categoria.java
@@ -1,5 +1,7 @@
 package com.estudo.algamoney.api.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -18,7 +20,8 @@ import lombok.experimental.Tolerate;
 @Entity
 @EqualsAndHashCode
 @Table(name = "categoria")
-public class Categoria {
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/estudo/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/estudo/algamoney/api/resource/CategoriaResource.java
@@ -1,14 +1,20 @@
 package com.estudo.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudo.algamoney.api.model.Categoria;
 import com.estudo.algamoney.api.repository.CategoriaRepository;
@@ -25,12 +31,18 @@ public class CategoriaResource {
 		return categoriaRepository.findAll();
 	}
 	
-	@GetMapping("/{id}")
-	public Optional<Categoria> buscarPorId(@PathVariable Long id) {
-		return categoriaRepository.findById(id);
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPorId(@PathVariable Long codigo) {
+		return categoriaRepository.findById(codigo);
 	}
 	
-	public void criar(@RequestBody Categoria categoria) {
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo})")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 }
